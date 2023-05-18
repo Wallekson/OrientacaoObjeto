b96df696bb417bea1b150b197efa657d0dfba617
Wallekson <walisoncastor@gmail.com>
2023-05-18 06:54:33 -0300

    Exercise 1/2

--- Exercicios/Exercicio1/src/main/java/entities/Retangle.java
+++ Exercicios/Exercicio1/src/main/java/entities/Retangle.java
@@ -6,7 +6,7 @@ public class Retangle {
 
     public double width;
 
-    //public double diagonal;
+    public double diagonal;
 
     public double Area(){
         return height * width;
@@ -16,16 +16,19 @@ public class Retangle {
         return (width + height) * 2;
     }
 
-//    public double Diagonal(double diagonal){
-//        this.diagonal =
-//    }
+    public double Diagonal(){
+       return diagonal = Math.sqrt(Math.pow(height,2) + Math.pow(width,2));
+    }
 
     public String toString() {
         return "AREA = "
                 + String.format("%.2f" , Area())
                 + String.format("%n")
                 + "PERIMETER = "
-                + String.format("%.2f" , Perimeter());
+                + String.format("%.2f" , Perimeter())
+                + String.format("%n")
+                + "DIAGONAL = "
+                + String.format("%.2f" , Diagonal());
 
     }
 
